cdf675654e84e636a5efcc4700e8f94f41f0475a
joumaamhamad <mhmdjom198@gmail.com>
2024-03-04 12:52:04 +0200

    add.env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 mongoose.connect('mongodb://127.0.0.1/portfolio').then
 ( 
@@ -49,7 +51,7 @@ app.post('/send-email', (req, res) => {
 
 
 
-app.listen(5000, () => {
+app.listen(port, () => {
 
     console.log(`Server is running on port 5000`);
 });
